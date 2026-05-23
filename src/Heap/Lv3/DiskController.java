86428b6e670882919d97d96293581e8fa7c439c2
Park Chan Jin <qarkchanjin@gmail.com>
2026-05-23 13:24:27 +0900

    디스크 컨트롤러 - 힙을 사용해서 푸는 문제, 우선순위를 잘 따지며 시간 관리를 잘 해야하는 문제 내 생각대로 푼 풀이 → 시간 초과 나는 코드

---
+++ src/Heap/Lv3/DiskController.java
@@ -0,0 +1,94 @@
+package Heap.Lv3;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class DiskController {
+    public static void main(String[] args) {
+//        int[][] jobs = {{0, 3}, {1, 9}, {3, 5}};
+        int[][] jobs = {{1, 9}, {0, 3}, {3, 5}};
+
+        System.out.println(solution(jobs));
+    }
+
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+
+        // 요청 시점에 따라 정렬
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Job> queue = new PriorityQueue<>();
+        int time = 0; //시작 시점
+        int waitCount = 0; //대기 큐에 들어간 개수
+        int count = 0; // 작업 완료한 디스크 개수
+
+        while (count < jobs.length) {
+            if(waitCount < jobs.length) {
+                for (int i = waitCount; i < jobs.length; i++) {
+                    if (time == jobs[i][0]) { //요청 시점이랑 같으면 넣는다.
+                        queue.offer(new Job(jobs[i][1], jobs[i][0], i));
+                    }
+                }
+
+                waitCount += queue.size();
+            }
+
+
+            if(!queue.isEmpty()) {
+                Job job = queue.poll();
+                time = time + job.jobTime;
+
+                if(waitCount < jobs.length) {
+                    for (int i = waitCount; i < jobs.length; i++) {
+                        if (time >= jobs[i][0]) { //요청 시점보다 작을 경우에 넣는다 (이유: 하드 작업이 끝난 시점으로 시간을 건너 뛰어서
+                            queue.offer(new Job(jobs[i][1], jobs[i][0], i));
+                        }
+                    }
+                    waitCount += queue.size();
+                }
+                count++;
+
+                answer += time - job.reqTime;
+            }
+        }
+
+        return answer / count;
+    }
+
+    static class Job implements Comparable<Job> {
+        int jobTime;
+        int reqTime;
+        int jobNum;
+
+        public Job(int jobTime, int reqTime, int jobNum) {
+            this.jobTime = jobTime;
+            this.reqTime = reqTime;
+            this.jobNum = jobNum;
+        }
+
+        @Override
+        public int compareTo(Job o) {
+            if (this.jobTime != o.jobTime) {
+                return this.jobTime - o.jobTime; // 1. 작업 시간 오름차순
+            } else if (this.reqTime != o.reqTime) {
+                return this.reqTime - o.reqTime; // 2. 요청 시각 오름차순
+            } else {
+                return this.jobNum - o.jobNum;   // 3. 작업 번호 오름차순
+            }
+        }
+    }
+}
+
+/*
+우선 순위 디스크 컨트롤러
+1. 작업 번호, 작업 요청 시각, 작업의 소요 시간을 저장해두는 대기 큐가 있다 (처음엔 비어있음)
+2. 대기큐가 비어있지 않다면, 가장 우선 순위가 높은 작업을 대기 큐에서 꺼내 하드에게 그 작업을 시킴
+(이때, 작업의 소요 시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것으로 우선 순위가 높다.)
+3. 하드는 작업이 시작하면 끝날 때까지 그 작업만 수행
+4. 마치는 시점과 다른 작업이 들어오는 시점이 겹칠 경우, 디스크 컨트롤러에서 들어온 작업을 대기 큐에 저장한 뒤 우선순위가 높은 작업을 대기 큐에서 꺼내서 하드에 작업을 시킨다.
+
+주요 포인트 1. 작업을 시작하면, 작업이 끝날 떄까지 하드는 주어진 일만한다 >> 작업이 끝났을 때의 시점 중요
+주요 포인트 2. 작업의 소요 시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것으로 우선 순위 구분
+(클래스 하나 만들어서 Comparator 정의 후 하는 것이 맞을까?)
+주요 포인트 3. 시점을 중요하기 때문에, 시간 추적 필요 (시뮬)
+*/
