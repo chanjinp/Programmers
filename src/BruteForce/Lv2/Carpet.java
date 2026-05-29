a6b761e5f865b6268f656171113c650388f7b8ad
Park Chan Jin <qarkchanjin@gmail.com>
2026-05-29 22:14:47 +0900

    카펫 - 넓이와, 가로는 항상 세로보다 크거나 같아야한다는 것을 수식화하여, 넓이와의 상관 관계를 나타내며, for문 조건을 효율적으로 개선하는 방안

--- src/BruteForce/Lv2/Carpet.java
+++ src/BruteForce/Lv2/Carpet.java
@@ -15,11 +15,11 @@ public class Carpet {
 
         int size = brown + yellow;
 
-        for(int i = 3; i < brown - 3; i++) {
-            int width = i;
+        for(int h = 3; h <= size / h; h++) {
+            int height = h;
 
-            if (size % width == 0) {
-                int height = size / width;
+            if (size % height == 0) {
+                int width = size / height;
 
                 int yellowSize = (width - 2) * (height - 2);
 
@@ -53,4 +53,15 @@ brown이 테두리를 담당하기 떄문에 길이의 경우 brown의 개수만
 
 즉, 가로의 길이를 3으로 두고 시작하면 height의 길이는 brown의 개수가 되며,
 중간에 yellow가 올려면 width - 2 * height - 2의 값이 yello의 개수와 같아져야 한다
+
+조금 더 최적화할 수 있는 방법 추가.
+
+넓이는 가로 * 세로 => Area = w * h
+
+단 조건이 w >= h이기 때문에 w = Area / h >= h와 동일하며, Area >= h^2  → sqrt(Area) >= h 이런 공식이 나온다.
+
+이런 공식을 이용했을 때 세로든 가로든 어차피 마지막은 가로가 무조건 길어야하는 조건이며, 넓이에 대해 위와 같이 나오기 때문에
+for문의 최대 조건을 좀 수정할 수 있다.
+
+좀 더 수식화 + 효율적인 코드가 완성된다.
 */
